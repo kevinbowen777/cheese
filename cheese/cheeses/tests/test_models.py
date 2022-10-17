fe69bb795d1f454e360daead3cfdaaee845c3f8c
kevinbowen777 <kevin.bowen@gmail.com>
2022-10-17 16:43:11 -0700

    Re-order imports. Improve formatting

--- cheese/cheeses/tests/test_models.py
+++ cheese/cheeses/tests/test_models.py
@@ -1,16 +1,18 @@
 import pytest
 
-from ..models import Cheese
 from .factories import CheeseFactory
+from ..models import Cheese  # noqa:F401
 
 
 # Connects our tests with our database
 pytestmark = pytest.mark.django_db
 
+
 def test__str__():
     cheese = CheeseFactory()
     assert cheese.__str__() == cheese.name
     assert str(cheese) == cheese.name
+
 
 def test_get_absolute_url():
     cheese = CheeseFactory()
